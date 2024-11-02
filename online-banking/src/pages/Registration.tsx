5ebac9cb782ada0ee3f74b53d508469afb853b9b
YenDo-2001 <dyen0901@gmail.com>
2024-11-01 23:13:15 -0700

    Registration.tsx

--- online-banking/src/pages/Registration.tsx
+++ online-banking/src/pages/Registration.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './styles.css';
+import './Registration.css';
 
 const Registration = () => {
     const [LastName, setLastName] = useState<string>('');
