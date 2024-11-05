a9fdb72c8619c5901465ef2a8c8b460adca900d3
YenDo-2001 <dyen0901@gmail.com>
2024-11-05 14:16:57 -0800

    ResetPassword.tsx

---
+++ online-banking/src/pages/ResetPassword.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ResetPassword = () => {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setErrorMsg('Passwords do not match. Please try again.');
+      return;
+    }
+
+    console.log('Updating password to:', newPassword);
+    setSuccessMsg('Password has been successfully updated!');
+    setTimeout(() => navigate('/'), 2000);
+  };
+
+  return (
+    
+         <div style={{fontFamily: 'Arial, sans-serif', margin: 0 }}>
+      <header>
+        <nav style={{
+                    backgroundColor: '#003459',
+                    padding: '30px',
+                    color: 'white',
+                    textAlign: 'center'
+                }}>
+          <h1 style={{fontWeight: 'bold', fontSize: '30px'}}>Bank of Banks</h1>
+        </nav>
+      </header>
+
+
+      <div 
+        style={{
+          width: '300px',
+          margin: '100px auto',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          padding: '20px',
+          borderRadius: '10px',
+        }}>
+
+<h2 style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '25px', marginTop: '40px'}}>Reset Password</h2>
+<form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+<div style={{ width: '100%' }}>
+            <label style={{ display: 'block', marginBottom: '5px' , fontWeight: 'bold'}}>New Password:</label>
+            <input
+              style={{
+                padding: '10px',
+                backgroundColor: '#D3D3D3',
+                fontSize: '15px',
+                borderRadius: '4px',
+                border: '1px solid #809AAC',
+                marginBottom: '15px',
+                width: '93%'
+              }}
+              type="password"
+              name="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              id="newPassword"
+              required
+            />
+          </div>
+    
+
+
+
+        <div style={{ width: '100%' }}>
+            <label style={{ display: 'block', marginBottom: '5px' , fontWeight: 'bold'}}>Confirm New Password:</label>
+            <input
+              style={{
+                padding: '10px',
+                backgroundColor: '#D3D3D3',
+                fontSize: '15px',
+                borderRadius: '4px',
+                border: '1px solid #809AAC',
+                marginBottom: '15px',
+                width: '93%'
+              }}
+
+              type="password"
+              name="confirmNewpassword"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e.target.value)}
+              id="confirmPassword"
+              required
+            />
+          </div>
+
+
+          <button 
+            style={{
+              width: '100px',
+              padding: '10px',
+              backgroundColor: '#003459',
+              color: 'white',
+              border: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              textAlign: 'center',
+              marginTop: '10px',
+              fontSize: '15px'
+            }}>Submit</button>
+          <br />
+          {successMsg && <div className="success-message">{successMsg}</div>}
+          {errorMsg && <div className="error-message">{errorMsg}</div>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
