c9603e8621e3b9b1305f52e1424b07226aac8a0d
Bjorneee <benicimo4@gmail.com>
2024-11-21 02:42:07 -0800

    Updated CSS to new homepage content

--- online-banking/src/textdescriptions.js
+++ online-banking/src/textdescriptions.js
@@ -3,3 +3,12 @@ export const savings_desc =
 
 export const checkings_desc = 
     "Simplify your finances with the Bank of Banks Checking Account, designed for convenience, flexibility, and control over your money."
+
+export const credit_desc =
+    "Enjoy unmatched benefits and top-tier features that will empower you to achieve your financial goals."
+
+export const insurance_desc =
+    "Our insurance benefits provide robust protection for you, your family, and your assets, ensuring you're prepared for life's uncertainties."
+
+export const business_desc =
+    "Empower your business with the tailored solutions and comprehensive support offered by Bank of Banks Business Accounts."
