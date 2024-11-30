1f8eb03e0fd5c4184caa8192a0fb735392441c79
yyaatt0 <yathien122@gmail.com>
2024-11-30 01:35:07 -0800

    created the looks for the account settings, but need to add the functionality for each of the buttons found in there

--- online-banking/src/images.js
+++ online-banking/src/images.js
@@ -4,13 +4,15 @@ import home_cover from './images/cover-img.jpg';
 import home_signing from './images/home_signing.jpg';
 import checking from './images/checking.jpg';
 import saving from './images/saving.jpg';
+import user_icon from './images/user_icon.jpg'
 
 // Add [image name] here one imported
 const images = {
     home_cover,
     home_signing,
     checking,
-    saving
+    saving,
+    user_icon
 }
 
 // > import images from '../images'; < to access images
