4c4d4eb0dc463cd0b876e9c776c68e8458d59ecb
YenDo-2001 <dyen0901@gmail.com>
2024-11-29 22:34:46 -0800

    UserLogin.tsx
    updated navbar

--- online-banking/src/pages/UserLogin.tsx
+++ online-banking/src/pages/UserLogin.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import NavBar from '../components/NavBar';
+import UserBar from '../components/UserBar';
 import FooterCard from '../components/FooterCard';
 
 const UserLogin = () => {
